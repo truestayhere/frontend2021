5172e976b600149bfc7cbdd50a4914dbe8550d7d
Polina Malinina <bgp922@myy.haaga-helia.fi>
2021-09-28 20:16:29 +0300

    Added switch

--- src/App.js
+++ src/App.js
@@ -8,14 +8,31 @@ function App() {
 
   const [index, setIndex] = useState(0)
 
+  // code source: https://www.w3schools.com/howto/howto_js_treeview.asp ----
+  const [toggler, setToggler] = useState(document.getElementsByClassName("caret"))
+
+  for (let i = 0; i < toggler.length; i++) {
+    setToggler(...toggler, toggler[i].addEventListener("click", function () {
+      this.parentElement.querySelector(".nested").classList.toggle("active")
+      this.classList.toggle("caret-down")
+    }))
+  }
+  // ---------------------
+
   const switchAssignments = () => {
     switch (index) {
       case 0:
         return (
           <div>
-            <h2>Create React App</h2>
-            <button onClick={() => setIndex(1)}>Todos</button>
-            <button onClick={() => setIndex(2)}>Todos with delete</button>
+            <h2>React</h2>
+            <ul id="myUL">
+              <li><span className="caret">Todos</span>
+                <ul className="nested">
+                  <li><button onClick={() => setIndex(1)}>Exercise 12</button></li>
+                  <li><button onClick={() => setIndex(2)}>Exercise 13</button></li>
+                </ul>
+              </li>
+            </ul>
           </div>
         )
       case 1:
