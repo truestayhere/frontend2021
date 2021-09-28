d0bf952881b17873f4ac65f64293bf6c5e86e3db
Polina Malinina <bgp922@myy.haaga-helia.fi>
2021-09-28 18:17:32 +0300

    Final version

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,22 +1,27 @@
 import React, { useState } from 'react'
 
 function Todos() {
-    const [desc, setDesc] = useState('')
+    const [todo, setTodo] = useState({ desc: '', date: '' })
     const [todos, setTodos] = useState([])
 
+
     const addTodo = () => {
-        setTodos([...todos, desc])
+        setTodos([...todos, todo])
     }
 
+    const inputChanged = (event) => {
+        setTodo({ ...todo, [event.target.name]: event.target.value })
+    }
 
     return (
         <div>
-            <input value={desc} onChange={e => setDesc(e.target.value)} />
+            <input name="desc" value={todo.desc} onChange={inputChanged} />
+            <input name="date" value={todo.date} onChange={inputChanged} />
             <button onClick={addTodo}>Add</button>
             <table>
                 <tbody>
                     {
-                        todos.map((todo, index) => <tr key={index}><td>{todo}</td></tr>)
+                        todos.map((todo, index) => <tr key={index}><td>{todo.desc} {todo.date}</td></tr>)
                     }
                 </tbody>
             </table>
